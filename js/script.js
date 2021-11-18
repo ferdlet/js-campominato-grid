7664967539c27e49073d624d3b9c4ff7beda4f44
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-18 13:42:20 +0100

    clicked

--- js/script.js
+++ js/script.js
@@ -2,45 +2,57 @@ const container = document.querySelector('.container');
 
 const buttonGioca = document.getElementById('gioca');
 
+container.innerHTML = '';
+
+for (let i = 0; i < 100; i++) {
+    const cell = document.createElement('div');
+    cell.className = 'square easy';
+    cell.innerHTML = i + 1;
+    cell.addEventListener('click', function () {
+        cell.classList.add('clicked-true');
+    });
+    container.appendChild(cell);
+}
+
 buttonGioca.addEventListener("click", function () {
-    
-    let items = '';
+
+    container.innerHTML = '';
 
     const difficulty = document.getElementById('difficulty').value;
-    console.log(difficulty);
 
     switch (difficulty) {
         case "easy":
-            for (let i = 1; i <= 100; i++) {
-                items += `
-                <div class="square easy"><span>${i}</span></div>
-            `;
+            for (let i = 0; i < 100; i++) {
+                const cell = document.createElement('div');
+                cell.className = 'square easy';
+                cell.innerHTML = i + 1;
+                cell.addEventListener('click', function () {
+                    cell.classList.add('clicked-true');
+                });
+                container.appendChild(cell);
             }
-            container.innerHTML = items;
             break;
         case "hard":
-            for (let i = 1; i <= 81; i++) {
-                items += `
-                <div class="square hard"><span>${i}</span></div>
-            `;
+            for (let i = 0; i < 81; i++) {
+                const cell = document.createElement('div');
+                cell.className = 'square hard';
+                cell.innerHTML = i + 1;
+                cell.addEventListener('click', function () {
+                    cell.classList.add('clicked-true');
+                });
+                container.appendChild(cell);
             }
-            container.innerHTML = items;
             break;
         case "expert":
-            for (let i = 1; i <= 49; i++) {
-                items += `
-                <div class="square expert"><span>${i}</span></div>
-            `;
+            for (let i = 0; i < 49; i++) {
+                const cell = document.createElement('div');
+                cell.className = 'square expert';
+                cell.innerHTML = i + 1;
+                cell.addEventListener('click', function () {
+                    cell.classList.add('clicked-true');
+                });
+                container.appendChild(cell);
             }
-            container.innerHTML = items;
             break;
     }
 });
-
-const square = document.getElementsByClassName('square');
-
-// for (i = 1; i <= 100; i++) {
-//     square[i].addEventListener("click", function(){
-
-//     })
-// }
